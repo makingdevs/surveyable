49b8e4fd525ed516ba3f215106baec3bcdb1a03b
Jorge Acosta <jorge@makingdevs.com>
2014-03-04 12:02:51 -0600

    coding #3 se cambio ruta de archivos

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -8,8 +8,8 @@ modules = {
   estilos{
     resource url:'css/bootstrap-responsive.min.css'
     resource url:'css/bootstrap.min.css'
-    resource url:'images/glyphicons-halflings-white.png'
-    resource url:'images/glyphicons-halflings.png'
+    resource url:'img/glyphicons-halflings-white.png'
+    resource url:'img/glyphicons-halflings.png'
     resource url:'js/bootstrap.min.js'
   }
 

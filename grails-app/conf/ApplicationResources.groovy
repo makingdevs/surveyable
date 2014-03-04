87cef18b5c68cdc1aa746831574b1705c9253db7
Jorge Acosta <jorge@makingdevs.com>
2014-03-04 11:11:11 -0600

    coding #3 se creo modulos BT

---
+++ grails-app/conf/ApplicationResources.groovy
@@ -0,0 +1,16 @@
+modules = {
+
+  validation{
+    resource url:'js/jquery.validate.min.js'
+    resource url:'js/questionary/validation.js'
+  }
+  
+  estilos{
+    resource url:'css/bootstrap-responsive.min.css'
+    resource url:'css/bootstrap.min.css'
+    resource url:'images/glyphicons-halflings-white.png'
+    resource url:'images/glyphicons-halflings.png'
+    resource url:'js/bootstrap.min.js'
+  }
+
+}

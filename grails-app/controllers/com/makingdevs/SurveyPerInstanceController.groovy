2866fd5d8378ce676a2d5ac70af10a870fdaff1d
Jorge Acosta <jorge@makingdevs.com>
2014-03-06 17:09:54 -0600

    coding #9 se implementa metodo para guardar respuestas en un surveyPerInstance

--- grails-app/controllers/com/makingdevs/SurveyPerInstanceController.groovy
+++ grails-app/controllers/com/makingdevs/SurveyPerInstanceController.groovy
@@ -9,11 +9,52 @@ class SurveyPerInstanceController {
   def answerSurvey(){
     def survey=Survey.get(params.id)
     def surveyPerInstance=surveyPerInstanceService.instanceSurvey(survey.id)
-    [surveyPerInstance:surveyPerInstance]
+    [surveyPerInstance:surveyPerInstance,
+    numPreguntas:surveyPerInstance.answerPerInstances.size()]
   }
 
   def guardarSurvey(){
-
+    def idPregunta = []
+    def idPreguntas= []
+    def respuestas=[]
+    def satisfaccion=[]
+    def observaciones=[]
+    for (int i = 0; i < params.numPreguntas.toLong(); i++) {
+      idPregunta << params.getAt("question[${i}]").id
+    }
+    def preguntas=Question.getAll(idPregunta)
+    def surveyPerInstance = SurveyPerInstance.get(params.num)
+    for (int i = 0; i < params.numPreguntas.toLong(); i++) {
+      switch(preguntas.getAt(i).questionType) {
+        case QuestionType.CON_CALIFICACION:
+          idPreguntas << params.getAt("question[${i}]").id
+          respuestas << params.getAt("question[${i}]").description
+          satisfaccion << params.getAt("question[${i}]").rango
+          observaciones << params.getAt("question[${i}]").comentario
+        break
+        case QuestionType.SIN_CALIFICACION:
+          idPreguntas << params.getAt("question[${i}]").id
+          respuestas << params.getAt("question[${i}]").description
+          satisfaccion << null
+          observaciones << params.getAt("question[${i}]").comentario
+        break
+        case QuestionType.ABIERTA:
+          idPreguntas << params.getAt("question[${i}]").id
+          respuestas << params.getAt("question[${i}]").description
+          satisfaccion << null
+          observaciones << null
+        break
+      }
+    }
+    for (int i = 0; i < params.numPreguntas.toLong(); i++) {
+      surveyPerInstanceService.addAnswer(idPregunta.getAt(i),respuestas.getAt(i),satisfaccion.getAt(i),observaciones.getAt(i),surveyPerInstance.id)
+    }
+    redirect(action: "showSurveyPerInstance", params: [surveyPerInstance: surveyPerInstance])
   }
 
+  def showSurveyPerInstance(){
+    [surveyPerInstance:params.surveyPerInstance]
+  }
+    
 }
+

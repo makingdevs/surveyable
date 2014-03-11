fab3e7a65b6636b6829a447b198130f182282abf
Jorge Acosta <jorge@makingdevs.com>
2014-03-11 10:50:17 -0600

    coding #14 se agrego metodo a servicio

--- grails-app/services/com/makingdevs/SurveyPerInstanceService.groovy
+++ grails-app/services/com/makingdevs/SurveyPerInstanceService.groovy
@@ -39,4 +39,46 @@ class SurveyPerInstanceService {
     surveyPerInstance.save()
   }
 
+  def saveAnswersInSurvey(def params){
+
+    def idPregunta = []
+    def idPreguntas= []
+    def respuestas=[]
+    def satisfaccion=[]
+    def observaciones=[]
+    for (int i = 0; i < params.numPreguntas.toLong(); i++) {
+      idPregunta << params.getAt("question[${i}]").id
+    }
+    def preguntas=Question.getAll(idPregunta)
+    def surveyPerInstance = SurveyPerInstance.get(params.num)
+    for (int i = 0; i < params.numPreguntas.toLong(); i++) {
+      switch(preguntas.getAt(i).questionType) {
+        case QuestionType.CON_CALIFICACION:
+          idPreguntas << params.getAt("question[${i}]").id
+          respuestas << params.getAt("question[${i}]").description
+          satisfaccion << params.getAt("question[${i}]").rango
+          observaciones << params.getAt("question[${i}]").comentario
+        break
+        case QuestionType.SIN_CALIFICACION:
+          idPreguntas << params.getAt("question[${i}]").id
+          respuestas << params.getAt("question[${i}]").description
+          satisfaccion << null
+          observaciones << params.getAt("question[${i}]").comentario
+        break
+        case QuestionType.ABIERTA:
+          idPreguntas << params.getAt("question[${i}]").id
+          respuestas << params.getAt("question[${i}]").description
+          satisfaccion << null
+          observaciones << null
+        break
+      }
+    }
+    for (int i = 0; i < params.numPreguntas.toLong(); i++) {
+      addAnswer(idPregunta.getAt(i),respuestas.getAt(i),satisfaccion.getAt(i),observaciones.getAt(i),surveyPerInstance.id)
+    }
+    surveyPerInstance.surveyPerInstanceStatus=SurveyPerInstanceStatus.CONTESTADO
+    surveyPerInstance
+
+  }
+
 }

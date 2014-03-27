a6b42b6472c0a8ed66f3a0800179d5256053f099
Jorge Acosta <jorge@makingdevs.com>
2014-03-27 17:14:34 -0600

    coding #22 sustituye a aplicationResources

---
+++ grails-app/conf/SurvayableResources.groovy
@@ -0,0 +1,20 @@
+modules = {
+
+  validation{
+    resource url:'js/jquery.validate.min.js'
+    resource url:'js/questionary/validation.js'
+  }
+  
+  estilos{
+    resource url:'css/bootstrap-responsive.min.css'
+    resource url:'css/bootstrap.min.css'
+    resource url:'img/glyphicons-halflings-white.png'
+    resource url:'img/glyphicons-halflings.png'
+    resource url:'js/bootstrap.min.js'
+  }
+
+  jquery{
+  resource url:'js/jquery-2.1.0.min.js'
+  }
+
+}
